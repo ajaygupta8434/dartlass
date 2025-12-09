58395bb22e591063a9eb4b6f8aea9085c102842c
Ajay Gupta <148214882+ajaygupta8434@users.noreply.github.com>
2025-12-09 22:53:24 +0530

    helo

--- helo.dart
+++ helo.dart
@@ -1,3 +1,3 @@
 void main(){
- print("hello world");
+ print("Dart is an open-source, general-purpose programming language developed by Google. It is designed for building modern, scalable, and high-performance applications for web, mobile, and desktop. Dart is particularly known for being the language behind the Flutter framework, which is used for developing cross-platform mobile applications.");
 }

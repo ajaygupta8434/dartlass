c02ead4a58bc0bcb09fa61e7e385b258759fb83f
Ajay Gupta <148214882+ajaygupta8434@users.noreply.github.com>
2025-12-11 01:00:09 +0530

    hello

---
+++ projecrt2.dart
@@ -0,0 +1,53 @@
+
+import 'dart:io';
+  List<Map<String ,dynamic>> StudentList=[];
+ void main(){
+  int i;
+  Map<String, int>marks={};
+  int total=0;
+  while(true){
+    stdout.write("enter student name");
+    String name= stdin.readLineSync()!;
+       stdout.write("enter roll number");
+       int roll=int.parse(stdin.readLineSync()!);
+       stdout.write("enter marks");
+       for(i=1;i<=5;i++){
+         stdout.write("Sub $i :");
+          int m =int.parse(stdin.readLineSync()!);
+           marks["sub$i"]=m;
+           
+            total +=m;
+
+       }
+        double per=total/5;
+          
+    
+       
+    
+    Map<String,dynamic> student={
+      "name":name,
+         "roll":roll,
+         "marks":marks,
+         "total":total,
+         "percantage":per
+      };
+      StudentList.add(student);
+       print("=====Student Details==========\n");
+       for(var s in StudentList){
+        print("Name : ${s['name']}\n Roll : ${s['roll']}\n Marks : ${s['marks']}\nTotal :${s['name']}\nPercantages: ${s['percantage']} ");
+          if(per>=60){
+            print("First");
+          }
+          else if(per>=45){
+            print("Second");
+          }
+          else if(per>=33){
+              print("Third");
+          }
+          else{
+               print("Failed");
+          }
+       }
+        
+  }
+}

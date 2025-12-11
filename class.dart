a689576903122e19660fda415e6f937f7290467b
Ajay Gupta <148214882+ajaygupta8434@users.noreply.github.com>
2025-12-12 01:20:09 +0530

    hello

---
+++ class.dart
@@ -0,0 +1,31 @@
+//Simple class
+// class Hello{
+//  void display(){
+//   print("hello worls!");
+//  }
+// }
+//  void main(){
+//   Hello h1 =Hello();
+//   h1.display();
+//  }
+
+//Abstract class
+// abstract class Student {
+//   void display();
+// }
+// class Ajay extends Student{
+//    @override
+//    void display(){
+//     print("my self ajay gupta");
+//    }
+//    void show(){
+//     print("i pursing MCA at amity ");
+//    }
+// }
+//  void main(){
+//   Ajay a1=Ajay();
+//   a1.show();
+//   a1.display();
+//  }
+
+
